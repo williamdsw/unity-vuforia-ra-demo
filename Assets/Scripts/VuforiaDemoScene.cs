460bab957a7ebc346a7e135079fa537e9712988f
William Santos <williamdsw@outlook.com>
2021-01-28 10:44:29 -0300

    :ok_hand: Adding back button to Vuforia Demo Scene

---
+++ Assets/Scripts/VuforiaDemoScene.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VuforiaDemoScene : MonoBehaviour
+{
+    // || Inspector References
+
+    [Header("UI Canvas Elements")]
+    [SerializeField] private Button backButton;
+
+    private void Awake()
+    {
+        CheckReferences();
+        SetEventListeners();
+    }
+
+    private void CheckReferences()
+    {
+        if (!backButton)
+        {
+            throw new Exception("BackButton is null!");
+        }
+    }
+
+    private void SetEventListeners()
+    {
+        backButton.onClick.AddListener(() => GotoNextScene(GlobalReferences.ScenesNames.IntroScene));
+    }
+
+    private void GotoNextScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
