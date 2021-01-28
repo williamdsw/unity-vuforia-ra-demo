97556b4a0b06879400370fb0af34d2129c0adae1
William Santos <williamdsw@outlook.com>
2021-01-28 10:33:04 -0300

    :sparkles: Intro scene and importing assets

---
+++ Assets/Scripts/GlobalReferences.cs
@@ -0,0 +1,8 @@
+﻿public class GlobalReferences
+{
+    public class ScenesNames
+    {
+        public static string IntroScene => "IntroScene";
+        public static string VuforiaDemoScene => "VuforiaDemoScene";
+    }
+}
---
+++ Assets/Scripts/IntroScene.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroScene : MonoBehaviour
+{
+    // || Inspector References
+
+    [Header("UI Canvas Elements")]
+    [SerializeField] private Button beginButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        CheckReferences();
+        SetEventListeners();
+    }
+
+    private void CheckReferences()
+    {
+        if (!beginButton)
+        {
+            throw new Exception("BeginButton is null!");
+        }
+
+        if (!quitButton)
+        {
+            throw new Exception("QuitButton is null!");
+        }
+    }
+
+    private void SetEventListeners()
+    {
+        beginButton.onClick.AddListener(() => GotoNextScene(GlobalReferences.ScenesNames.VuforiaDemoScene));
+        quitButton.onClick.AddListener(OnQuit);
+    }
+
+    private void GotoNextScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void OnQuit()
+    {
+        Application.Quit();
+    }
+}
